e8fdd2539120393fa3dad837a204a1c207334f4e
skazgor <ruhulazgor@gmail.com>
2023-12-07 10:48:49 +0600

    assignment 1

--- Assignment 1/image_reconstruction.py
+++ Assignment 1/image_reconstruction.py
@@ -8,17 +8,6 @@ def low_rank_approximation(A, k):
     A_k = np.dot(U[:, :k], np.dot(diagonal_matrix, Vt[:k, :]))
     return A_k
 
-def choose_smallest_k(A, threshold):
-    U, Sigma, Vt = np.linalg.svd(A, full_matrices=False)
-    total_sum = np.sum(Sigma)
-
-    cumulative_sum = np.cumsum(Sigma)
-    cumulative_percentage_sum = cumulative_sum / total_sum
-
-    k= np.argmax(cumulative_percentage_sum >= threshold)+1
-
-    return k
-
 
 # Read the grayscale image
 image = cv2.imread('image.jpg', cv2.IMREAD_GRAYSCALE)
@@ -27,17 +16,11 @@ image = cv2.imread('image.jpg', cv2.IMREAD_GRAYSCALE)
 # Perform Singular Value Decomposition
 U, Sigma, Vt = np.linalg.svd(image, full_matrices=False)
 
-threshold = 0.65
-k_smallest = choose_smallest_k(image, threshold)
-
-
-
 # Vary the value of k and plot the resultant k-rank approximations
 # first 10 values of k is spaced by 5. starting from 1. then spaced by 50 to the end
 # rank
-k= min(image.shape)
+k= Sigma.size
 k_values = np.hstack((np.arange(1, min(k,50), 5), np.arange(100, k, 50)))
-k_values= np.hstack((k_values, k_smallest))
 
 image_rows=k_values.size//4
 if(k_values.size%4!=0):
